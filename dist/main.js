18c4d1867ef3b87aac31d850091c36a02f2146a1
김도현 <do@tecalog.com>
2021-03-16 12:41:45 +0900

    gitginore update

--- dist/main.js
+++ dist/main.js
@@ -1,16 +1,26 @@
 /******/ (() => { // webpackBootstrap
 /******/ 	"use strict";
 var __webpack_exports__ = {};
+// This entry need to be wrapped in an IIFE because it uses a non-standard name for the exports (exports).
+(() => {
+var exports = __webpack_exports__;
 /*!*********************!*\
   !*** ./app/main.ts ***!
   \*********************/
 
+Object.defineProperty(exports, "__esModule", ({ value: true }));
 var Main = /** @class */ (function () {
     function Main() {
     }
+    Main.prototype.start = function () {
+        console.log("Hello");
+    };
     return Main;
 }());
-window["Main"] = new Main();
+var main = new Main();
+main.start();
+
+})();
 
 /******/ })()
 ;
